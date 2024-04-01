07e3716601b6a2087acb7ed10355245502ac6f9f
f.bakker@minfin.nl <f.bakker@minfin.nl>
2024-04-01 21:49:36 +0200

    WIP: added diagram functionality

--- Tools/Playground/Playground.py
+++ Tools/Playground/Playground.py
@@ -37,6 +37,7 @@ template_graph        = readStringFromFile(directory_path + "OntoReSpec/Specific
 html_serialisation    = readStringFromFile(directory_path + "htmlvoc/Specification/html - core.ttl")
 html_vocabulary       = readStringFromFile(directory_path + "htmlvoc/Specification/html - core.ttl")
 manchester_vocabulary = readStringFromFile(directory_path+"OntoManchester/Specification/manchestersyntax.ttl")
+mermaid_vocabulary    = readStringFromFile(directory_path+"OntoMermaid/Specification/mermaid.ttl")
 
 def generateManchester(manchester_generator, serializable_graph):
         
@@ -216,6 +217,267 @@ def generateReSpecRDF(shaclgraph, serializable_graph):
       
         return serializable_graph
 
+
+def generateDiagram(mermaid_generator, serializable_graph):
+        
+        # call PyShacl engine and apply the HTML vocabulary to the serializable HTML document
+        pyshacl.validate(
+        data_graph=serializable_graph,
+        shacl_graph=mermaid_generator,
+        data_graph_format="turtle",
+        shacl_graph_format="turtle",
+        advanced=True,
+        inplace=True,
+        inference=None,
+        iterate_rules=False, #rather than setting this to true, it is better to do the iteration in the script as PyShacl seems to have some buggy behavior around iteration.
+        debug=False,
+        )
+        
+       
+        statusquery = serializable_graph.query('''
+            
+prefix mermaid: <https://data.rijksfinancien.nl/mermaid/model/def/>
+prefix owl: <http://www.w3.org/2002/07/owl#>
+prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix sh: <http://www.w3.org/ns/shacl#>
+ASK
+WHERE {
+  # Any OWL or RDFS entity that is not yet described in terms of the manchester syntax
+  {
+    $this a owl:Class.
+  }  
+  UNION
+  {
+    $this a rdfs:Class.
+  }
+  UNION
+  {
+    $this rdfs:subClassOf []
+  }
+  UNION
+  {
+    $this owl:equivalentClass []
+  }
+  UNION
+  {
+    $this owl:unionOf []
+  }
+  UNION
+  {
+    $this owl:intersectionOf []
+  }
+  UNION
+  {
+    $this owl:complementOf []
+  }
+  UNION
+  {
+    $this owl:oneOf []
+  }
+  UNION
+  {
+    $this owl:allValuesFrom []
+  }
+  UNION
+  {
+    $this owl:someValuesFrom []
+  }
+  UNION
+  {
+    $this owl:hasValue []
+  }
+  UNION
+  {
+    $this owl:cardinality []
+  }
+  UNION
+  {
+    $this owl:maxCardinality []
+  }
+  UNION
+  {
+    $this owl:minCardinality []
+  }  
+  UNION
+  {
+   $this rdf:type rdf:Property
+  }
+  UNION
+  {
+    $this rdf:type owl:DatatypeProperty.
+  }
+  UNION
+  {
+    $this rdf:type owl:ObjectProperty.
+  }
+  UNION
+  {
+    $this rdfs:subPropertyOf []
+    FILTER NOT EXISTS {$this rdf:type owl:AnnotationProperty}.
+    FILTER NOT EXISTS {$this rdf:type owl:InverseFunctionalProperty}
+    FILTER NOT EXISTS {$this rdf:type owl:FunctionalProperty}
+  }
+  UNION
+  {
+    $this owl:equivalentProperty [].
+  }
+  filter not exists {
+    $this mermaid:syntax 'CLASS'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'CLASS'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'SUBCLASSOF'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'EQUIVALENTTO'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'OR'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'AND'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'NOT'.
+  }
+  filter not exists {
+    $this mermaid:syntax '{}'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'ONLY'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'SOME'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'VALUE'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'EXACTLY'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'MAX'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'MIN'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'RDF_PROPERTY'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'DATATYPEPROPERTY'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'OBJECTPROPERTY'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'SUBPROPERTYOF'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'EQUIVALENTPROPERTY'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'OR-DATATYPE'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'AND-DATATYPE'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'NOT-DATATYPE'.
+  }
+  filter not exists {
+    $this mermaid:syntax '{}-DATATYPE'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'EXACTLYQUALIFIED'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'MAXQUALIFIED'.
+  }
+  filter not exists {
+    $this mermaid:syntax 'MINQUALIFIED'.
+  }
+}
+        ''')   
+
+        resultquery = serializable_graph.query('''
+            
+prefix owl: <http://www.w3.org/2002/07/owl#>
+prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix sh: <http://www.w3.org/ns/shacl#>
+prefix : <https://data.rijksfinancien.nl/mermaid/model/def/>
+
+SELECT (GROUP_CONCAT(?label; separator="\\n") AS ?mermaid_code)
+WHERE {
+  ?element :label ?label.
+  
+  minus {
+      ?this owl:annotatedTarget ?target.
+      ?target (:|!:)* ?element.
+      ?element :label ?label
+      FILTER isBlank(?target)
+      FILTER isBlank(?element)}
+  
+}
+ORDER BY ?mermaid_code
+        ''')   
+
+        # Check whether another iteration is needed. If every OWL and RDFS construct contains a mermaid:syntax statement, the processing is considered done.
+        for status in statusquery:
+            if status == True:
+                generateDiagram(mermaid_generator, serializable_graph)
+            else:     
+                 for result in resultquery:
+                    mermaid_code = result["mermaid_code"]
+                    output_file_path = directory_path+"OntoReSpec/Tools/Playground/static/diagram.html"
+                    # Create the HTML content with the Mermaid code
+                    html_start =  '''
+                    <!DOCTYPE html>
+                    <html>
+                    <head>
+                    </head>
+                    <body>
+                    <div><pre class="mermaid">
+                    %%{
+                    init: {
+                    "flowchart":{
+                    "useMaxWidth": 0
+                    }
+                    }
+                    }%%
+                    graph TB
+                    classDef Datatype fill:#9c6,stroke:#9c6;
+                    
+                    
+                    '''
+                    
+                    html_graph = f'''
+                  
+                    {mermaid_code}
+                    
+                    ''' 
+                    
+                    html_end = '''
+                    </pre>
+                    <script type="module">
+                      import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
+                      mermaid.initialize({ startOnLoad: true });
+                    </script>
+                    </div>
+                    </body>
+                    </html>
+                    '''
+                    
+                    html_content = html_start + html_graph + html_end
+                    
+                    # Write the HTML content to the output file
+                    with open(output_file_path, "w", encoding="utf-8") as file:
+                        file.write(html_content) 
+
 def generateHTML(shaclgraph, serializable_graph):
         
         # call PyShacl engine and apply the html vocabulary to the serializable html document
@@ -321,6 +583,9 @@ def generateReSpec():
 
     for result in ontologyQuery:
         generationGraph.add((doc[generation_iri], dct.subject, URIRef(result.ontology)))
+    
+    # Generating diagram 
+    generateDiagram(mermaid_vocabulary, generationGraph)
     
     # Enriching the ontology with manchester metadata
     generationGraph = generateManchester(manchester_vocabulary, generationGraph)
